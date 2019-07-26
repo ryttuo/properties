fd5bbc2388e0a3ede561655586f0557cb45fda4a
Andres Solano S <andres.solano@gmail.com>
2019-07-26 07:41:06 -0600

    set title into articles for global store

--- src/app/amb-articles/amb-articles.component.ts
+++ src/app/amb-articles/amb-articles.component.ts
@@ -3,6 +3,8 @@ import { Store, select } from '@ngrx/store';
 import { ArticleStoreInterface, getArticles } from './store/article.selectors';
 import { LoadArticlesAction } from './store/article.actions';
 import { Observable } from 'rxjs';
+import { AppState } from '../core/appState';
+import { SetAppTitle } from '../core/layout/layout.actions';
 
 @Component({
   selector: 'amb-articles',
@@ -15,9 +17,12 @@ export class AmbArticlesComponent implements OnInit {
 
   constructor(
     private articleStore: Store<ArticleStoreInterface>,
+    private layoutStore: Store<AppState>
   ) { }
 
   ngOnInit() {
+
+    this.layoutStore.dispatch(new SetAppTitle('articles'));
 
     this.articleStore.dispatch(new LoadArticlesAction());
 
